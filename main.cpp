c17912c2595abb9c1496f5866b92ff08b9ea3b71
PavlovaSofiiaV <sofapavlova344@gmail.com>
2026-03-04 16:39:47 +0200

    Додала клас book до main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
+#include "book.h"
 #include <iostream>
 
-int main() {
 
+int main() {
+    book book1;
+    book book2("The Inheritance Games");
+    book book3("The Inheritance Games", "Jennifer Lynn Barnes");
+    book book4("The Inheritance Games", "Jennifer Lynn Barnes", 2020);
+    book book5("The Inheritance Games", "Jennifer Lynn Barnes", 2020, 384);
+
+    book1.aboutbook();
+    book2.aboutbook();
+    book3.aboutbook();
+    book4.aboutbook();
+    book5.aboutbook();
     return 0;
 }
