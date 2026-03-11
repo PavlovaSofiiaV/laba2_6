97c1f54737b050c9c444acb08eb193bffdd86cfc
PavlovaSofiiaV <sofapavlova344@gmail.com>
2026-03-11 17:39:04 +0200

    Add this for Employee class

--- employee.cpp
+++ employee.cpp
@@ -8,24 +8,24 @@ Employee::Employee() {
     work_days=0;
 }
 Employee::Employee(std::string new_name) {
-    name=new_name;
+    this->name=new_name;
     salary=0;
     work_days=0;
 }
 Employee::Employee(std::string new_name, int new_salary) {
-    name=new_name;
-    salary=new_salary;
+    this->name=new_name;
+    this->salary=new_salary;
     work_days=0;
 }
 Employee::Employee(std::string new_name, int new_salary,int new_work_days) {
-    name=new_name;
-    salary=new_salary;
-    work_days=new_work_days;
+    this->name=new_name;
+    this->salary=new_salary;
+    this->work_days=new_work_days;
 }
 Employee::Employee( int new_salary, int new_work_days) {
     name="None";
-    salary=new_salary;
-    work_days=new_work_days;
+    this->salary=new_salary;
+    this->work_days=new_work_days;
 }
 Employee::~Employee(){}
 void Employee::about_employee() {
--- employee.h
+++ employee.h
@@ -13,10 +13,10 @@ private:
     int salary;
 public:
     Employee();
-    Employee(std::string new_name);
-    Employee(std::string new_name, int new_salary);
-    Employee(std::string new_name, int new_salary, int new_work_days);
-    Employee(int new_salary, int new_work_days);
+    Employee(std::string name);
+    Employee(std::string name, int salary);
+    Employee(std::string name, int salary, int work_days);
+    Employee(int salary, int work_days);
     ~Employee();
 
     void about_employee();
